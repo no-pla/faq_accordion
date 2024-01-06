915c0e726334b97913fbda82f102996976d384e7
no-pla <songjihyun323@gmail.com>
2024-01-06 15:40:43 +0900

    Design: Fix Style overflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <main className="bg-lightPink h-screen flex justify-center items-center">
+    <main className="bg-lightPink min-h-screen h-max flex justify-center items-center overflow-scroll sm:py-6">
       <Card />
     </main>
   );
